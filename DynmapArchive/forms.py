1b232241b9d2426083d825451d168e93d61424dc
Zackaree <72373956+Zackareee@users.noreply.github.com>
2021-06-21 19:13:54 +1000

    Date fix

--- DynmapArchive/forms.py
+++ DynmapArchive/forms.py
@@ -2,14 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms.fields import SubmitField, StringField, FieldList,FormField
 from wtforms.validators import InputRequired,Optional
 from wtforms.fields.html5 import DateField
+import os
+
+if os.name == 'nt':
+    s = "#"
+else:
+    s = "-"
+
+
+
 
 class TownForm(FlaskForm):
     town = StringField(validators=[InputRequired()])
 
 class MapForm(FlaskForm):
-  date = DateField('Start Date')
+  date = DateField('Start Date',format=F"%{s}d.%{s}m.%{s}y")
   town = FieldList(FormField(TownForm), min_entries=1, validators=[Optional()])
-  datetwo = DateField('End Date (Optional)', validators=[Optional()])
+  datetwo = DateField('End Date (Optional)',format=F"%{s}d.%{s}m.%{s}y", validators=[Optional()])
   addrow = SubmitField('Add row')
   delrow = SubmitField('Delete row')
   submit = SubmitField("Render Town(s)")
